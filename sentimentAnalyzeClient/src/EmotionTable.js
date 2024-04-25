acac8ab563499a6efaa8865cf2f36c2fa660e927
Abhi84sia <Abhishek84sia1726@gmail.com>
2024-04-25 03:59:11 -0400

    changes made from theia lab environment

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -2,22 +2,38 @@ import React from 'react';
 import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
-    render() {
-      //Returns the emotions as an HTML table
-      return (  
-        <div>
-          <table className="table table-bordered">
-            <tbody>
+  render() {
+    // Define the emotions and their sentiment scores
+    const emotions = ['sadness', 'joy', 'fear', 'disgust', 'anger'];
+    const sentimentScores = [0.250524, 0.722962, 0.009157, 0.003132, 0.016098];
+
+    const emotionScores = emotions.map((emotion, index) => {
+      return { emotion, score: sentimentScores[index] };
+    });
+
+    return (
+      <div>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Emotion</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
             {
-              /*Write code to use the .map method that you worked on in the 
-              Hands-on React lab to extract the emotions. If you are stuck,
-              please click the instructions to see how to implement a map*/
+              emotionScores.map((emotionScore, index) => (
+                <tr key={index}>
+                  <td>{emotionScore.emotion}</td>
+                  <td>{emotionScore.score}</td>
+                </tr>
+              ))
             }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+
 export default EmotionTable;
